2f0a5e03aca012168c387829c17d4b450b66dcc9
VictorM <neuron96@mail.ru>
2022-06-23 16:18:46 +0400

    develop reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n < 0
+        ? n.toString().split("").reverse().join("").slice(0, -1)
+        : n.toString().split("").reverse().join("");
+};
